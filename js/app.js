4032b72be2b65c2352503e6d63a38840a798e161
Juan <espinozaajuan04@gmail.com>
2021-10-09 16:08:52 -0300

    IDs agregados y arreglos en app.js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const ingresos = [
+    new Ingreso('Sueldo', 60000.00),
+    new Ingreso ('Trabajos Freelance', 30000.00)
+];
+
+const egresos = [
+    new Egreso ('Pago Alquiler', 15000.00),
+    new Egreso ('Gastos de comida', 8000.00)
+];
+
+let cargarApp = ()=>{
+    cargarCabecero();
+}
+
+let cargarCabecero = ()=>{
+    
+}
