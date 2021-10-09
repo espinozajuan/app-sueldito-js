f5700041c2e1479a5b82a20d57e26b51b43f6f72
Juan <espinozaajuan04@gmail.com>
2021-10-09 16:15:38 -0300

    Funcion agregada para calculo presupuesto, porcentaje, ingresos y egresos

--- js/app.js
+++ js/app.js
@@ -12,6 +12,27 @@ let cargarApp = ()=>{
     cargarCabecero();
 }
 
+let totalIngresos = ()=>{
+    let totalIngreso = 0;
+    for (let ingreso of ingresos){
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = ()=>{
+    let totalEgreso = 0;
+    for (let egreso of egresos){
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
 let cargarCabecero = ()=>{
-    
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    document.getElementById('presupuesto').innerHTML = presupuesto;
+    document.getElementById('porcentaje').innerHTML = porcentajeEgreso;
+    document.getElementById('ingresos').innerHTML = totalIngresos();
+    document.getElementById('egresos').innerHTML = totalEgresos();
 }
