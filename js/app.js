2b07de047e8227b617ebca4734c6463e56ef9da8
Juan <espinozaajuan04@gmail.com>
2021-10-09 18:36:12 -0300

    funcionalidad terminada, falta responsive

--- js/app.js
+++ js/app.js
@@ -63,13 +63,20 @@ const crearIngresoHTML = (ingreso)=>{
         <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)}</div>
         <div class="elemento_eliminar">
             <button class='elemento_eliminar--btn'>
-                <ion-icon name="close-circle-outline"></ion-icon>
+                <ion-icon name="close-circle-outline" onclick="eliminarIngreso(${ingreso.id})"></ion-icon>
             </button>
         </div>
     </div>
 </div>
     `;
     return ingresoHTML;
+}
+
+const eliminarIngreso = (id)=>{
+    let indiceEliminar = ingresos.findIndex(ingreso => ingreso.id === id);
+    ingresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarIngresos();
 }
 
 const cargarEgresos = ()=>{
@@ -95,11 +102,37 @@ const crearEgresoHTML = (egreso)=>{
         </div>
         <div class="elemento_eliminar">
             <button class="elemento_eliminar--btn">
-                <ion-icon name="close"></ion-icon>
+                <ion-icon name="close" onclick="eliminarEgreso(${egreso.id})"></ion-icon>
             </button>
         </div>
     </div>
 </div>
     `;
     return egresoHTML;
+}
+
+const eliminarEgreso = (id) => {
+    let indiceEliminar = egresos.findIndex(egreso => egreso.id === id);
+    egresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarEgresos();
+}
+
+let agregarDato = () => {
+    let formulario = document.forms['formulario'];
+    let tipo = formulario['tipo'];
+    let descripcion = formulario['descripcion'];
+    let valor = formulario['valor'];
+    if(descripcion.value !== '' && valor.value !== ''){
+        if(tipo.value === 'ingreso'){
+            ingresos.push( new Ingreso(descripcion.value, +valor.value));
+            cargarCabecero();
+            cargarIngresos();
+        }
+        else if (tipo.value === 'egreso'){
+            egresos.push( new Egreso(descripcion.value, +valor.value));
+            cargarCabecero();
+            cargarEgresos();
+        }
+    }
 }
